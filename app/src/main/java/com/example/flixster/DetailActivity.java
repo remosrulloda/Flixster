68b1bebee9c1129c124e9aeb0382245ef5c777c3
remosrulloda <remosrulloda@gmail.com>
2021-09-23 13:41:18 -0700

    Changes done

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -24,7 +24,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "AIzaSyCOqGXvKFl8rLH0o3L9piKp-_0_gL3Yo44";
+    private static final String YOUTUBE_API_KEY = BuildConfig.YOUTUBE_KEY;
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
